3d792171aaa0d265f0a5778eb2fff80c8276048a
jeilers96 <jeilers96@gmail.com>
2019-03-16 17:09:55 -0500

    create functional craousel component and start styling carousel

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -4,30 +4,48 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-carousel',
   template: `
-    <div>
-      <div *ngFor = "let slide of slides">
-        <img [src]="slide.image">
-        <div>
-          <h2>{{slide.name}}</h2>
-          {{slide.description}}
+    <div class="carousel-container">
+      <ng-container *ngFor="let slide of slides">
+        <div *ngIf="slide.id == slideIndex">
+          <img [src]="slide.image">
+          <div>
+            <h2>{{slide.name}}</h2>
+            {{slide.description}}
+          </div>
         </div>
-      </div>
+      </ng-container>
+      <a class="prev" (click)="plusSlides(-1)">&#10094;</a>
+      <a class="next" (click)="plusSlides(1)">&#10095;</a>
     </div>
   `,
   styleUrls: ['./carousel.component.less']
 })
 export class CarouselComponent implements OnInit {
 
-  slides: CarouselSlide;
+  slides: CarouselSlide[];
+  slideIndex: integer;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get('http://localhost:4200/assets/carousel.json')
+    this.http.get('../assets/carousel.json')
       .subscribe(data => {
         console.log(data)
         this.slides = data;
     });
+    this.slideIndex = 1;
+  }
+
+  plusSlides(slideVariation: integer) {
+    const slideMod = slideVariation + this.slideIndex;
+
+    if(slideMod < 1) {
+      this.slideIndex = this.slides.length;
+    } else if(slideMod > this.slides.length) {
+      this.slideIndex = 1;
+    } else {
+      this.slideIndex = slideVariation + this.slideIndex;
+    }
   }
 }
 
