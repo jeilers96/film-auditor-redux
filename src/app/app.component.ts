533cd6c3fe6667731d63a8ab82ad97f20b70db8d
jeilers96 <jeilers96@gmail.com>
2019-06-20 18:03:52 -0500

    remove unneeded code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent {
   title = 'The flim Auditor';
-  loading = true;
-
-  ngAfterViewInit() {
-    this.loading = false;
-}
 }
